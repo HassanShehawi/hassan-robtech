8de72c894ae9d46c274e346fe5860eedc5b598a9
tudeng <hassan.shehawi@gmail.com>
2017-05-12 14:00:10 +0300

    Lab before final

---
+++ tr.cpp
@@ -0,0 +1,100 @@
+ // Planning to a Pose goal
+  geometry_msgs::Pose target_pose1;
+  target_pose1.orientation.w = 1.0;
+  target_pose1.position.x = 0.28;
+  target_pose1.position.y = -0.7;
+  target_pose1.position.z = 1.0;
+  group.setPoseTarget(target_pose1);
+
+  moveit::planning_interface::MoveGroup::Plan my_plan;
+  bool success = group.plan(my_plan);
+
+  ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
+  /* Sleep to give Rviz time to visualize the plan. */
+  sleep(5.0);
+
+  if (1)
+  {
+    ROS_INFO("Visualizing plan 1 (again)");    
+    display_trajectory.trajectory_start = my_plan.start_state_;
+    display_trajectory.trajectory.push_back(my_plan.trajectory_);
+    display_publisher.publish(display_trajectory);
+    /* Sleep to give Rviz time to visualize the plan. */
+    sleep(5.0);
+  }
+  
+
+  // Planning to a joint-space goal 
+  std::vector<double> group_variable_values;
+  group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()), group_variable_values);
+
+  group_variable_values[0] = -1.0;  
+  group.setJointValueTarget(group_variable_values);
+  success = group.plan(my_plan);
+
+  ROS_INFO("Visualizing plan 2 (joint space goal) %s",success?"":"FAILED");
+
+  sleep(5.0);
+
+
+  // Planning with Path Constraints
+  moveit_msgs::OrientationConstraint ocm;  
+  ocm.link_name = "r_wrist_roll_link";  
+  ocm.header.frame_id = "base_link";
+  ocm.orientation.w = 1.0;
+  ocm.absolute_x_axis_tolerance = 0.1;
+  ocm.absolute_y_axis_tolerance = 0.1;
+  ocm.absolute_z_axis_tolerance = 0.1;
+  ocm.weight = 1.0;
+  
+  moveit_msgs::Constraints test_constraints;
+  test_constraints.orientation_constraints.push_back(ocm);  
+  group.setPathConstraints(test_constraints);
+
+  robot_state::RobotState start_state(*group.getCurrentState());
+  geometry_msgs::Pose start_pose2;
+  start_pose2.orientation.w = 1.0;
+  start_pose2.position.x = 0.55;
+  start_pose2.position.y = -0.05;
+  start_pose2.position.z = 0.8;
+  const robot_state::JointModelGroup *joint_model_group =
+                  start_state.getJointModelGroup(group.getName());
+  start_state.setFromIK(joint_model_group, start_pose2);
+  group.setStartState(start_state);
+  
+  group.setPoseTarget(target_pose1);
+  success = group.plan(my_plan);
+
+  ROS_INFO("Visualizing plan 3 (constraints) %s",success?"":"FAILED");
+  sleep(10.0);
+
+  group.clearPathConstraints();
+
+  // Cartesian Paths
+  std::vector<geometry_msgs::Pose> waypoints;
+
+  geometry_msgs::Pose target_pose3 = start_pose2;
+  target_pose3.position.x += 0.2;
+  target_pose3.position.z += 0.2;
+  waypoints.push_back(target_pose3);  // up and out
+
+  target_pose3.position.y -= 0.2;
+  waypoints.push_back(target_pose3);  // left
+
+  target_pose3.position.z -= 0.2;
+  target_pose3.position.y += 0.2;
+  target_pose3.position.x -= 0.2;
+  waypoints.push_back(target_pose3);  // down and right (back to start)
+
+  moveit_msgs::RobotTrajectory trajectory;
+  double fraction = group.computeCartesianPath(waypoints,
+                                               0.01,  // eef_step
+                                               0.0,   // jump_threshold
+                                               trajectory);
+
+  ROS_INFO("Visualizing plan 4 (cartesian path) (%.2f%% acheived)",
+        fraction * 100.0);    
+  /* Sleep to give Rviz time to visualize the plan. */
+  sleep(15.0);
+
+
